e34d6adf6544964b7c985ca6bce02fe424dd99b9
tohoangkhoi <keepsmilingtg@gmail.com>
2024-01-02 16:31:41 +0700

    feat: change repo name

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ AppDataSource.initialize()
   .then(async () => {
     console.log("Inserting a new user into the database...");
     const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
+    user.firstName = "Yasuo";
+    user.lastName = "Trash";
     user.age = 25;
     await AppDataSource.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
