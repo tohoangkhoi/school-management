16217d09b440b3c0b7974aa416bf9705ec277e67
tohoangkhoi <keepsmilingtg@gmail.com>
2024-01-03 12:05:18 +0700

    feat: replace db url with db crendetials

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
-const { DB_URL } = process.env;
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "ep-late-boat-63999583.ap-southeast-1.aws.neon.tech",
-  port: 5432, // Replace with your actual port
-  username: "tohoangkhoi",
-  password: "YJkr8gOvGL0j",
-  database: "small-erb-db",
+  host: DB_HOST,
+  port: parseInt(DB_PORT || "5432", 10), // Parse as an integer, replace with your actual port
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
   ssl: true,
   entities: [User],
   migrations: ["migration/*.ts"],
-
   subscribers: [],
 });
